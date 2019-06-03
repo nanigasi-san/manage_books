3dedc58b3ef5b56271e20e8b193996617e190ad1
nanigasi <nanigasi.py@gmail.com>
2019-06-03 20:53:57 +0900

    検索機能の実装

--- web/books.py
+++ web/books.py
@@ -1,9 +1,22 @@
-from flask import Blueprint,render_template
+from flask import Blueprint,render_template,request
 
 bp = Blueprint("books",__name__)
-@bp.route("/")
+@bp.route("/",methods=["GET","POST"])
 def index():
-    return render_template("index.html",books=get_testdata())
+    if request.method == "POST":
+        genre = request.form["genre"]
+        message = '分野が「{0}」を含む本は'.format(genre)
+        
+        searchdata = search_testdata("genre",genre)
+        if len(searchdata)>0:
+            message += "以下の通りです"
+        else:
+            searchdata = get_testdata()
+            message += "見つかりませんでした"
+        return render_template("index.html",message=message,books=searchdata)
+    else:
+        message="お探しの分野は何ですか"
+        return render_template("index.html",message=message,books=get_testdata())
 
 def get_testdata():
     return [
@@ -14,3 +27,7 @@ def get_testdata():
     {"id":5,"title":"熱の解析的理論","author":"フーリエ","genre":"数学"},
     {"id":6,"title":"発微算法","author":"関考和","genre":"数学"},
     ]
+
+def search_testdata(key,sword):
+    alldata = get_testdata()
+    return [data for data in alldata if (sword in data[key])]

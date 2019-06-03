49412d0af397065666729f6a17a5de9d95705028
nanigasi <nanigasi.py@gmail.com>
2019-06-03 22:37:38 +0900

    DB不使用最高到達点 (#2)
    * 著者のbioに飛べるようにした
    
    内容にリンクを張れるようになった
    
    * アドレスの階層を作成した
    
    "/authors/ニュートン"の様にアクセスできるようにした

--- web/__init__.py
+++ web/__init__.py
@@ -1,9 +1,9 @@
 from flask import Flask
 def create_app():
     app = Flask(__name__)
-    from . import books
+    from . import books,authors
     app.register_blueprint(books.bp)
-
+    app.register_blueprint(authors.bp)
     @app.route("/test")
     def apptest():
         return "アプリケーションセットアップ完了！"

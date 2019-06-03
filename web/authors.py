d5e45e7fae3f0ed352ed5c962a3b41e2d642cc4e
nanigasi <nanigasi.py@gmail.com>
2019-06-03 22:23:14 +0900

    著者のbioに飛べるようにした
    内容にリンクを張れるようになった

---
+++ web/authors.py
@@ -0,0 +1,28 @@
+from flask import Blueprint,render_template
+
+bp = Blueprint("authors",__name__)
+def get_authors():
+    return [
+    {"author":"ニュートン",
+    "bio":"ニュートンです"},
+    {"author":"ラボアジェ",
+    "bio":"ラボアジェです"},
+    {"author":"ガウス",
+    "bio":"ガウスです"},
+    {"author":"コペルニクス",
+    "bio":"コペルニクスです"},
+    {"author":"フーリエ",
+    "bio":"フーリエです"},
+    {"author":"関考和",
+    "bio":"関考和です"},
+    ]
+
+def search_author(author):
+    alldata = get_authors()
+    for data in alldata:
+        if author==data["author"]:
+            return data
+
+@bp.route("/authors")
+def show():
+    return render_template("authors.html")

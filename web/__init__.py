fb8aea3d6450d9f75aec0ddbb38796252e14ae7d
nanigasi <nanigasi.py@gmail.com>
2019-06-04 09:04:02 +0900

    DBの表示

--- web/__init__.py
+++ web/__init__.py
@@ -1,11 +1,17 @@
 from flask import Flask
+import os
+
 def create_app():
     app = Flask(__name__)
     from . import books,authors
     app.register_blueprint(books.bp)
     app.register_blueprint(authors.bp)
-    @app.route("/test")
-    def apptest():
-        return "アプリケーションセットアップ完了！"
-        
+
+    from . import bookdb
+    bookdb.init_app(app)
+
+    app.config.from_mapping(
+    SECRET_KEY='temp',
+    DATABASE=os.path.join(app.instance_path,'bookdb.sqlite3'),
+    )
     return app

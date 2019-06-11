ecb868d9aa4445868f0cba1db1d8ea0eaf3208d7
nanigasi <nanigasi.py@gmail.com>
2019-06-11 17:41:55 +0900

    Post bot (#8)
    * discordから本を借りれるようにした
    
    * 本の確認ができるようになった
    
    * 本の貸し出し/返却の最終実装(#6)

---
+++ post/discord_bot.py
@@ -0,0 +1,50 @@
+import discord
+import requests
+import json
+
+TOKEN = "NTExNjk1MDQxODc4NTU2Njk0.XP9KQw.p1blUZuBk6LURPQtGCytbozHYP8"#アクセストークン
+client = discord.Client()
+def post_and_responce(data):
+    URL = "http://127.0.0.1:5000/rental"
+    res = requests.post(URL,data=data).json()
+    return res
+
+def get_books():
+    URL = "http://127.0.0.1:5000/books_data"
+    res = requests.get(URL).json()
+    return (res["data"],res["res"])
+
+@client.event
+async def on_ready():
+    userlist = client.users
+    for user in userlist:
+        print(user)
+
+@client.event
+async def on_message(msg):
+    channel = msg.channel
+    if msg.author == client.user:
+        return
+
+    if msg.content[:5] == "$rent":
+        book_title = msg.content[6:]
+        username = msg.author
+        data = {"lending": True,
+                "title": book_title}
+        res = post_and_responce(json.dumps(data))
+        await channel.send(str(res["res"]))
+
+    if msg.content[:7] == "$return":
+        book_title = msg.content[8:]
+        username = msg.author
+        data = {"lending": False,
+                "title": book_title}
+        res = post_and_responce(json.dumps(data))
+        await channel.send(str(res["res"]))
+
+    if msg.content == "$books":
+        books,_ = get_books()
+        book_str = "\n".join(books)
+        await channel.send(book_str)
+
+client.run(TOKEN)
